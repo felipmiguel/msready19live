b8ff4900ae34b697e36ee8e3d7aa9454be66fa78
Felip Miguel Puig <fmiguel@microsoft.com>
2019-07-11 15:37:13 +0200

    sample webapp java se

---
+++ demoseapp/src/test/java/com/msready19/raist213/demoseapp/DemoApplicationTests.java
@@ -0,0 +1,43 @@
+package com.msready19.raist213.demoseapp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	private String createURLWithPort(String uri) {
+		return "http://localhost:" + port + uri;
+	}
+
+	TestRestTemplate restTemplate = new TestRestTemplate();
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testEcho() {
+		String source = "Hello from ready!";
+		HttpEntity<String> entity = new HttpEntity<String>(source);
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/echo"), HttpMethod.POST, entity,
+				String.class);
+
+		String expected = "Spring echo: " + source;
+
+		assertEquals(expected, response.getBody());
+	}
+}
