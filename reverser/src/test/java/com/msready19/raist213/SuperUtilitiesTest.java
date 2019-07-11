6aeb8d164296d2ad4d0a08c7a671650e6d2770cd
Felip Miguel Puig <fmiguel@microsoft.com>
2019-07-11 06:23:57 +0200

    first part

---
+++ reverser/src/test/java/com/msready19/raist213/SuperUtilitiesTest.java
@@ -0,0 +1,17 @@
+package com.msready19.raist213;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SuperUtilitiesTest {
+
+    @Test
+    public void doReverse() {
+        String source = "abcde";
+        String expected = "edcba";
+        SuperUtilities r = new SuperUtilities();
+        String actual = r.doReverse(source);
+        assertEquals(expected, actual);
+    }
+}
