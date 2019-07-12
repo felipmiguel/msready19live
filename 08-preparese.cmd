f65db6a9881527181699d264c23c7a0014947569
Felip Miguel Puig <fmiguel@microsoft.com>
2019-07-12 12:03:37 +0200

    Real initial state

--- 08-preparese.cmd
+++
@@ -1,2 +0,0 @@
-copy /Y "C:\repos\msready19\complete\demoseapp\src\main\java\com\msready19\raist213\demoseapp\*.java" "C:\repos\msready19\live\demoseapp\src\main\java\com\msready19\raist213\demoseapp\"
-copy /Y "C:\repos\msready19\complete\demoseapp\src\test\java\com\msready19\raist213\demoseapp\*.java" "C:\repos\msready19\live\demoseapp\src\test\java\com\msready19\raist213\demoseapp\"
