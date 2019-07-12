d64e76274ae47ea1b43945727a540b7aabe378c6
Felip Miguel Puig <fmiguel@microsoft.com>
2019-07-12 12:01:33 +0200

    Initial status

--- demowebapp/src/main/java/com/msready19/raist213/ToolsService.java
+++ demowebapp/src/main/java/com/msready19/raist213/ToolsService.java
@@ -8,25 +8,10 @@ import javax.ws.rs.Produces;
 @Path("/tools")
 public class ToolsService {
 
-    private SuperUtilities _tools;
-
-    
-    public ToolsService(SuperUtilities tools) {
-        _tools = tools;
-    }
-
     @POST
     @Path("echo")
     @Produces("text/plain")
     public String echo(@FormParam("source") String source) {
         return source;
     }
-
-    @POST
-    @Path("reverse")
-    @Produces("text/plain")
-    public String reverse(@FormParam("source") String source) {
-        return _tools.doReverse(source);
-    }
-
 }
