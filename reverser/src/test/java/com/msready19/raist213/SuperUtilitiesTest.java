c2614f523c21e354ec079d4f0e02003e6cf74f34
Felip Miguel Puig <fmiguel@microsoft.com>
2019-07-16 00:15:25 +0200

    changes on reverser

--- reverser/src/test/java/com/msready19/raist213/SuperUtilitiesTest.java
+++ reverser/src/test/java/com/msready19/raist213/SuperUtilitiesTest.java
@@ -9,7 +9,7 @@ public class SuperUtilitiesTest {
     @Test
     public void doReverse() {
         String source = "abcde";
-        String expected = "edcba";
+        String expected = "reversed is edcba";
         SuperUtilities r = new SuperUtilities();
         String actual = r.doReverse(source);
         assertEquals(expected, actual);
